2dd0aab2f64048751312338e495a93aa9a9125ab
Jason Selwyn <jdselwyn@gmail.com>
2023-02-07 08:53:40 -0500

    add data

---
+++ Code/0 - preprocess metadata.R
@@ -0,0 +1,20 @@
+## Code to preprocess metadata into easier to use format
+
+#### Libraries ####
+library(tidyverse)
+
+preprocess_metadata <- read_csv('../Data/Florida_Census_outcomes.csv', 
+                                show_col_types = FALSE) %>%
+  select(-Genotype_Code) %>%
+  pivot_longer(cols = c(T3, T7),
+               names_to = 'time',
+               values_to = 'disease_state') %>%
+  janitor::clean_names() %>%
+  filter(!is.na(disease_state)) %>%
+  group_by(tank, treatment, genotype) %>%
+  filter(n() == 2) %>%
+  mutate(final_disease_state = disease_state[time == 'T7']) %>%
+  ungroup
+
+write_csv(preprocess_metadata, '../intermediate_files/preprocess_metadata.csv')
+
