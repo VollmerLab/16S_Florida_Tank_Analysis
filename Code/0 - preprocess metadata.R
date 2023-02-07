515fa90c2ecf17eec14457e8939c75fb34d60d59
Jason Selwyn <jdselwyn@gmail.com>
2023-02-07 09:59:04 -0500

    add microbiome preprocessing

--- Code/0 - preprocess metadata.R
+++ Code/0 - preprocess metadata.R
@@ -2,7 +2,10 @@
 
 #### Libraries ####
 library(tidyverse)
+library(magrittr)
 
+
+#### Read in & Process Data ####
 preprocess_metadata <- read_csv('../Data/Florida_Census_outcomes.csv', 
                                 show_col_types = FALSE) %>%
   select(-Genotype_Code) %>%
@@ -11,6 +14,7 @@ preprocess_metadata <- read_csv('../Data/Florida_Census_outcomes.csv',
                values_to = 'disease_state') %>%
   janitor::clean_names() %>%
   filter(!is.na(disease_state)) %>%
+  filter(!tank %in% c('H2')) %>% # Remove tanks which crashed due to anoxia
   group_by(tank, treatment, genotype) %>%
   filter(n() == 2) %>%
   mutate(final_disease_state = disease_state[time == 'T7']) %>%
@@ -18,5 +22,15 @@ preprocess_metadata <- read_csv('../Data/Florida_Census_outcomes.csv',
   mutate(sample_id = str_c(time, treatment, tank, genotype, sep = '_')) %>%
   select(sample_id, everything())
 
+#### Output preprocessed metadata ####
 write_csv(preprocess_metadata, '../intermediate_files/preprocess_metadata.csv')
 
+#### Simple analysis of outcome v treatment
+preprocess_metadata %>%
+  filter(time == 'T7') %>%
+  count(treatment, final_disease_state) %>%
+  pivot_wider(names_from = final_disease_state, 
+              values_from = n) %>%
+  column_to_rownames('treatment') %T>%
+  print %>%
+  chisq.test()
---
+++ Code/1 - preprocess microbiome.R
@@ -0,0 +1,54 @@
+## Code to preprocess microbiome sample data into easier to use format
+
+library(tidyverse)
+library(magrittr)
+library(phyloseq)
+
+#### Read in data ####
+preprocess_metadata <- read_csv('../intermediate_files/preprocess_metadata.csv', 
+                                show_col_types = FALSE)
+
+microbiome_raw <- read_rds('../Data/ps_fl_tank.rds') %>%
+  subset_taxa(Kingdom == "Bacteria" &
+                Phylum != "Cyanobacteria" &
+                !is.na(Phylum) &
+                Family != "Mitochondria" &
+                Class != "Chloroplast" & 
+                Order != "Chloroplast")
+
+#### Process Data ####
+#Split into repeated measures portion and input portion
+homogenate_samples <- sample_data(microbiome_raw) %>%
+  as_tibble(rownames = 'sample_id') %>%
+  filter(time %in% c('Acerv', 'T0')) %>%
+  mutate(retain_sample = TRUE)
+
+exposure_samples <- sample_data(microbiome_raw) %>%
+  as_tibble(rownames = 'sample_id') %>%
+  select(sample_id) %>%
+  inner_join(preprocess_metadata, by = 'sample_id') %>%
+  group_by(tank, treatment, genotype) %>% 
+  mutate(retain_sample = n() == 2) %>% #only keep samples when they are replicated at T3 & T7
+  ungroup %>%
+  rename(exposure = treatment)
+
+#Recombine and put data into sample_data slot of phyloseq object
+processed_microbiome <- microbiome_raw
+sample_data(processed_microbiome) <- bind_rows(homogenate_samples, exposure_samples) %>%
+  column_to_rownames('sample_id')
+processed_microbiome <- subset_samples(processed_microbiome, retain_sample)
+
+#### Output preprocessed microbiome as rds ####
+write_rds(processed_microbiome, '../intermediate_files/preprocess_microbiome.rds')
+
+
+#### Simple Analysis ####
+sample_data(processed_microbiome) %>%
+  as_tibble %>%
+  filter(time == 'T7') %>%
+  count(exposure, final_disease_state) %>%
+  pivot_wider(names_from = final_disease_state, 
+              values_from = n) %>%
+  column_to_rownames('exposure') %T>%
+  print %>%
+  chisq.test()
