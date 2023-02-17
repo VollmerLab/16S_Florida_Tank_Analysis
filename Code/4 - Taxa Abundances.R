f6cd4bdc052c751fc908d09492964b5fef6559e7
emilytrytten <trytten.e@northeastern.edu>
2023-02-17 15:54:28 -0500

    analyzed differences in taxon abundances

--- Code/4 - Taxa Abundances.R
+++ Code/4 - Taxa Abundances.R
@@ -1,3 +1,139 @@
+#code to examine changes to taxa abundances
+
+setwd("~/Documents/GitHub/16S_Florida_Tank_Analysis/Code")
+
+library(multcomp)
+library(phyloseq)
+library(microbiome)
+library(vegan)
+library(lme4)
+library(afex)
+library(emmeans)
+library(car)
+library(edgeR)
+library(metagMisc)
+library(ape)
+library(ggdist)
+library(gghalves)
+library(patchwork)
+library(tidyverse)
+
+select <- dplyr::select
+
+aggregation_level <- 'Family' #or none
+
+#### Read in Data ####
+microbiome_data <- read_rds("../intermediate_files/preprocess_microbiome.rds") %>%
+  subset_samples(time %in% c('T3', 'T7'))
+metadata <- sample_data(microbiome_data) %>%
+  as_tibble(rownames = 'sample_id') %>%
+  select(-retain_sample) %>%
+  mutate(fragment_id = str_c(str_replace_na(exposure, 'NA'), tank, genotype, sep = '_'),
+         .after = sample_id)
+
+if(aggregation_level != 'none'){
+  microbiome_data <- aggregate_taxa(microbiome_data, aggregation_level)
+  taxa_names(microbiome_data) <- str_replace_all(taxa_names(microbiome_data), ' |-', '_')
+} else {
+  taxa_names(microbiome_data) <- str_c('ASV', 1:length(taxa_names(microbiome_data)), sep = '_')
+}
+
+#### Filtering and normalizing data ####
+otu_tmm <- microbiome_data %>%
+  phyloseq_filter_prevalence(prev.trh = 0.1) %>%
+  otu_table() %>% 
+  as.data.frame %>%
+  as.matrix %>% 
+  DGEList(remove.zeros = TRUE) %>%
+  edgeR::calcNormFactors(method = 'TMMwsp') #change method to TMM
+
+
+cpm(otu_tmm, log = TRUE, prior.count = 2) %>%
+  rowMeans %>%
+  quantile(0.05)
+
+
+cpm(otu_tmm, log = TRUE, prior.count = 2) %>%
+  rowMeans %>%
+  tibble(x = .) %>%
+  ggplot(aes(x = x)) +
+  geom_histogram(fill = "grey", bins = 100) +
+  theme_classic() +
+  labs(y = "Density", x = "Filtered number of taxa (logCPM)",
+       title = "Distribution of normalized, filtered taxa")
+
+plot_pcoa <- function(cpm_counts){
+  filtered_pcoa <- t(cpm_counts) %>%
+    vegdist(method = 'euclidean') %>%
+    magrittr::divide_by(1000) %>%
+    pcoa()
+  
+  percent_variance <- filtered_pcoa$values$Eigenvalues / sum(filtered_pcoa$values$Eigenvalue)
+  
+  filtered_pcoa$vectors %>%
+    as_tibble(rownames = 'sample_id') %>%
+    dplyr::select(sample_id, Axis.1, Axis.2) %>%
+    inner_join(metadata,
+               by = 'sample_id') %>%
+    ggplot(aes(x = Axis.1, y = Axis.2, colour = final_disease_state, 
+               shape = time, group = fragment_id)) +
+    geom_point() +
+    geom_path() +
+    labs(x = str_c('PCoA 1 (', scales::percent(percent_variance[1]), ')'),
+         y = str_c('PCoA 2 (', scales::percent(percent_variance[2]), ')')) +
+    theme_classic()
+  
+}
+
+cpm(otu_tmm, log = TRUE, prior.count = 2) %>% plot_pcoa() #x axis explains greatest amount of variance
+  #y axis is next largest amount of variance
+  #cpm, when given a DGEList, defaults to applying normalization factors for us
+
+#nesting data by taxon abundances
+taxon_abundances <- cpm(otu_tmm, log = TRUE, prior.count = 2) %>%
+  t %>%
+  as_tibble(rownames = "sample_id") %>%
+  full_join(metadata, by = "sample_id") %>%
+  pivot_longer(cols = -any_of(colnames(metadata)), 
+               names_to = "taxon", values_to = "value") %>%
+  mutate(across(c(exposure, final_disease_state, time), factor)) %>%
+  nest_by(taxon)
+
+taxon_abundances$taxon
+data <- taxon_abundances$data[[31]]
+
+
+norm_mod <- lmer(value ~ time * (exposure + final_disease_state) + (1 | fragment_id), data = data) 
+
+rm_mod <- aov_4(value ~ time * (exposure + final_disease_state) + (time | fragment_id), data = data)
+
+gamma_mod <- glmer(value ~ time * (exposure + final_disease_state) + (1 | fragment_id), data = data, family = Gamma(link = log))
+
+bind_rows(
+  gamma = as_tibble(emmeans(gamma_mod, ~time:final_disease_state, type = 'response')) %>%
+    rename(emmean = response,
+           lower.CL = asymp.LCL,
+           upper.CL = asymp.UCL) ,
+  normal = as_tibble(emmeans(norm_mod, ~time:final_disease_state, type = 'response')),
+  rm = as_tibble(emmeans(rm_mod, ~time:final_disease_state, type = 'response')),
+  .id = 'model'
+) %>%
+  ggplot(aes(x = interaction(time, final_disease_state), y = emmean, ymin = lower.CL, ymax = upper.CL, colour = model)) +
+  geom_pointrange(position = position_dodge(0.5))
+  
+  
+#### Model Each Taxon Independently ####
+all_models <- taxon_abundances %>%
+  mutate(model = list(aov_4(value ~ time * (exposure + final_disease_state) + (time | fragment_id), data = data)))
+
+
+all_models %>%
+  reframe(model = list(model), 
+          sig_terms = find_unique_significant_terms(model, 0.05))
+
+#### Functions #####
+
+#needs to be tidied
 
 find_unique_significant_terms <- function(model, alpha){
   significant_terms <- model$anova_table %>%
@@ -17,11 +153,6 @@ make_emmean_model <- function(model, form, alpha){
   emmeans(model, specs = form, type = 'response') %>%
     cld(Letters = LETTERS, reversed = TRUE, alpha = alpha) 
 }
-
-
-predOut <- tmp$em_out[[1]]
-rawData<- tmp$data[[1]]
-var_inclusion <- tmp$terms[[1]]
 
 make_model_plot <- function(predOut, rawData, var_inclusion){
   
@@ -130,14 +261,14 @@ tmp <- all_models %>%
   mutate(plot = list(possibly(make_model_plot, otherwise = NULL, quiet = TRUE)(em_out, data, terms))) %>%
   group_by(taxon, data, model) %>%
   reframe(plot = ifelse(any(is.na(terms)),
-                          list(NULL),
-                          list(wrap_plots(plot) & 
-                                 labs(y = 'log2(CPM)') &
-                                 plot_annotation(title = taxon) & 
-                                 theme_classic() &
-                                 theme(panel.background = element_rect(colour = 'black', fill = NA),
-                                       axis.text = element_text(colour = 'black', size = 12),
-                                       axis.title = element_text(colour = 'black', size = 16))))) %>%
+                        list(NULL),
+                        list(wrap_plots(plot) & 
+                               labs(y = 'log2(CPM)') &
+                               plot_annotation(title = taxon) & 
+                               theme_classic() &
+                               theme(panel.background = element_rect(colour = 'black', fill = NA),
+                                     axis.text = element_text(colour = 'black', size = 12),
+                                     axis.title = element_text(colour = 'black', size = 16))))) %>%
   rowwise %>%
   mutate(possibly(make_aov_summary, otherwise = NULL)(model)) %>%
   ungroup 
