685b5f02225cfaaa50fb3f4e0e9b4fbdf9381bce
Jason Selwyn <jdselwyn@gmail.com>
2023-02-17 15:15:27 -0500

    Update code_sharing.R

--- Code/code_sharing.R
+++ Code/code_sharing.R
@@ -1,38 +1,112 @@
-cpm(gene_normalize_factors, log = TRUE, prior.count = 2) %>%
-  rowMeans %>%
-  quantile(0.05)
 
-
-cpm(gene_normalize_factors, log = TRUE, prior.count = 2) %>%
-  rowMeans %>%
-  tibble(x = .) %>%
-  ggplot(aes(x = x)) +
-  geom_histogram(fill = "grey", bins = 100) +
-  theme_classic() +
-  labs(y = "Density", x = "Filtered read counts (logCPM)",
-       title = "Distribution of normalized, filtered read counts")
-
-plot_pcoa <- function(cpm_counts){
-  filtered_pcoa <- t(cpm_counts) %>%
-    vegdist(method = 'euclidean') %>%
-    divide_by(1000) %>%
-    pcoa()
+find_unique_significant_terms <- function(model, alpha){
+  significant_terms <- model$anova_table %>%
+    as_tibble(rownames = 'param') %>%
+    janitor::clean_names() %>%
+    filter(pr_f < alpha) %>%
+    pull(param)
   
-  percent_variance <- filtered_pcoa$values$Eigenvalues / sum(filtered_pcoa$values$Eigenvalue)
+  unique_values <- outer(significant_terms, significant_terms, str_count) %>%
+    colSums() %>%
+    equals(1)
   
-  filtered_pcoa$vectors %>%
-    as_tibble(rownames = 'sequence_id') %>%
-    dplyr::select(sequence_id, Axis.1, Axis.2) %>%
-    inner_join(sample_metadata,
-               by = 'sequence_id') %>%
-    ggplot(aes(x = Axis.1, y = Axis.2, colour = treat_outcome, 
-               shape = timepoint, group = fragment_id)) +
-    geom_point() +
-    geom_path() +
-    labs(x = str_c('PCoA 1 (', scales::percent(percent_variance[1]), ')'),
-         y = str_c('PCoA 2 (', scales::percent(percent_variance[2]), ')')) +
-    theme_classic()
+  str_replace(significant_terms[unique_values], ':', '*')
+}
+
+make_emmean_model <- function(model, form, alpha){
+  emmeans(model, specs = form, type = 'response') %>%
+    cld(Letters = LETTERS, reversed = TRUE, alpha = alpha) 
+}
+
+make_model_plot <- function(predOut, rawData, var_inclusion){
+  
+  all_vars <- str_extract_all(var_inclusion, 'timepoint|final_disease_state|treatment') %>%
+    unlist
+  
+  if(any(str_detect(all_vars, 'time'))){
+    x_var <- str_subset(all_vars, 'time.*')
+  } else {
+    x_var <- all_vars[1]
+  }
+  
+  if(length(all_vars) > 1){
+    colour_var <- str_subset(all_vars, x_var, negate = TRUE)
+  } else {
+    colour_var <- NULL
+  }
+  
+  y_var <- str_subset(colnames(predOut), 'emmean|response')
+  y_data <- str_subset(colnames(rawData), 'gene_cpm|exon_cpm')
+  
+  if(is.null(colour_var)){
+    as_tibble(predOut) %>%
+      mutate(.group = str_trim(.group)) %>%
+      ggplot(aes(x = !!sym(x_var))) +
+      
+      stat_halfeye(data = rawData, aes(y = !!sym(y_data)),
+                   adjust = 0.5, width = 0.6, .width = 0, 
+                   alpha = 0.5, show.legend = FALSE,
+                   fatten_point = 0, justification = -0.25, 
+                   position = position_dodge(0.5), size = 0) +
+      
+      geom_half_point(data = rawData, aes(y = !!sym(y_data)),
+                      side = 'l', range_scale = 0.1, alpha = 1,
+                      position = position_dodge(width = 0.5),
+                      show.legend = FALSE,
+                      transformation = position_jitter(height = 0, width = 0.05)) +
+      
+      geom_pointrange(aes(y = !!sym(y_var),
+                          ymin = lower.CL, ymax = upper.CL),
+                      position = position_dodge(0.5)) +
+      geom_text(aes(y = (upper.CL), label = .group),
+                position = position_dodge(0.5), vjust = -0.1, 
+                show.legend = FALSE) +
+      labs(x = case_when(x_var %in% c('time', 'timepoint') ~ 'Time (d)',
+                         x_var == 'treatment' ~ 'Exposure',
+                         x_var == 'final_disease_state' ~ 'Disease State'))
+    
+  } else {
+    as_tibble(predOut) %>%
+      mutate(.group = str_trim(.group)) %>%
+      ggplot(aes(x = !!sym(x_var), colour = !!sym(colour_var))) +
+      
+      stat_halfeye(data = rawData, aes(y = !!sym(y_data), fill = !!sym(colour_var)),
+                   adjust = 0.5, width = 0.6, .width = 0, 
+                   alpha = 0.5, show.legend = FALSE,
+                   fatten_point = 0, justification = -0.25, 
+                   position = position_dodge(0.5), size = 0) +
+      
+      geom_half_point(data = rawData, aes(y = !!sym(y_data), colour = !!sym(colour_var)),
+                      side = 'l', range_scale = 0.1, alpha = 1,
+                      position = position_dodge(width = 0.5),
+                      show.legend = FALSE,
+                      transformation = position_jitter(height = 0, width = 0.05)) +
+      
+      geom_pointrange(aes(y = !!sym(y_var),
+                          ymin = lower.CL, ymax = upper.CL),
+                      position = position_dodge(0.5)) +
+      geom_text(aes(y = (upper.CL), label = .group),
+                position = position_dodge(0.5), vjust = -0.1,
+                show.legend = FALSE) +
+      labs(x = case_when(x_var %in% c('time', 'timepoint') ~ 'Time (d)',
+                         x_var == 'treatment' ~ 'Exposure',
+                         x_var == 'final_disease_state' ~ 'Disease State'),
+           colour = case_when(colour_var %in% c('time', 'timepoint') ~ 'Time (d)',
+                              colour_var == 'treatment' ~ 'Exposure',
+                              colour_var == 'final_disease_state' ~ 'Disease State'))
+  }
   
 }
 
-cpm(gene_normalize_factors, log = TRUE, prior.count = 2) %>% plot_pcoa()
+make_aov_summary <- function(model){
+  model$anova_table %>%
+    as_tibble(rownames = 'effect') %>%
+    janitor::clean_names() %>%
+    mutate(across(c(num_df, den_df), round, digits = 3),
+           df = str_c(num_df, den_df, sep = ', '),
+           p = pr_f) %>%
+    dplyr::select(effect, df, f, p, pes) %>%
+    pivot_wider(names_from = 'effect',
+                values_from = c('df', 'f', 'p', 'pes'), 
+                names_vary = 'slowest')
+}
