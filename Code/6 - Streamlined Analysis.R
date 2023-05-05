6f4293b56ddfad5c2c591104559f48add1f42b64
emilytrytten <trytten.e@northeastern.edu>
2023-05-05 10:26:18 -0400

    plot fixes

--- Code/6 - Streamlined Analysis.R
+++ Code/6 - Streamlined Analysis.R
@@ -225,14 +225,14 @@ upset(subset_asv_comp_upset,
   ggtitle("Likely Suspects")
   
 # All likely suspects - more abundant in Diseased
-upset(filter(subset_asv_comp_upset, d_v_h < 0) %>%
+cu_disease <- upset(filter(subset_asv_comp_upset, d_v_h < 0) %>%
         select(-d_v_h),  #negative = more in disease final state, positive = more in healthy disease state
       colnames(select(subset_asv_comp_upset, starts_with('p_'))), 
       
       annotations = list(
         # 2nd method - using ggplot
-        'Genus'=(
-          ggplot(mapping=aes(fill=Order)) 
+        'Family'=(
+          ggplot(mapping=aes(fill=Family)) 
           + geom_bar(stat = 'count', position = 'fill') 
           + scale_y_continuous(labels = scales::percent_format())
         ) +
@@ -240,28 +240,36 @@ upset(filter(subset_asv_comp_upset, d_v_h < 0) %>%
           theme(legend.position = 'top')
       ),
       
-      name='asv_names', width_ratio=0.1, min_size = 1) +
+      name='asv_names', width_ratio=0.1, min_size = 1) + #, max_size = 70
   ggtitle("More in Diseased")
 
 
 # All likely suspects - more abundant in Healthy
-upset(filter(subset_asv_comp_upset, d_v_h > 0) %>%
+cu_healthy <- upset(filter(subset_asv_comp_upset, d_v_h > 0) %>%
         select(-d_v_h),  #negative = more in disease final state, positive = more in healthy disease state
       colnames(select(subset_asv_comp_upset, starts_with('p_'))), 
       
       annotations = list(
         # 2nd method - using ggplot
-        'Genus'=(
-          ggplot(mapping=aes(fill=Genus)) 
+        'Family'=(
+          ggplot(mapping=aes(fill=Family)) 
           + geom_bar(stat = 'count', position = 'fill') 
           + scale_y_continuous(labels = scales::percent_format())
         ) +
-          ylab('Order') +
+          ylab('Family') +
           theme(legend.position = 'top')
       ),
       
-      name='asv_names', width_ratio=0.1, min_size = 1) +
+      name='asv_names', width_ratio=0.1, min_size = 1) + #, max_size = 30
   ggtitle("More in Healthy")
+
+
+layout <- '
+AA
+BB
+'
+wrap_plots(A = cu_disease, B = cu_healthy, design = layout)
+
 
 #### Graphing ####
 
