2b52cc9c8012a4cd42055f3c4b1c64cfdbc7fa78
Jason Selwyn <jdselwyn@gmail.com>
2023-06-13 11:13:06 -0400

    add rf model results

--- Code/4 - random forest analysis.R
+++ Code/4 - random forest analysis.R
@@ -1,6 +1,9 @@
 ## Goal - for disease exposed individuals predict final disease state based on T3 & T0 asv abundances
 ## Identify ASVs (and higher level groups most important to explaining final disease state
 
+##TODO - add in dimensionality reduction options (e.g. PLS & UMAP)?
+##TODO - add in other classifiers (e.g. naive bayes, discriminant analysis)?
+
 library(tidyverse)
 library(tidymodels)
 library(themis)
@@ -99,76 +102,54 @@ prepared_dataset <- samples_through_time %>%
               names_glue = "{timepoint}_{.value}") %>%
   mutate(final_disease_state = factor(final_disease_state))
 
-#### Plot PCA Contributions ####
-pca_results <- recipe(final_disease_state ~ ., data = prepared_dataset) %>%
-  update_role(genotype, new_role = "ID") %>%
-  step_nzv(all_predictors()) %>%
-  step_log(all_predictors(), base = 2, offset = 0.5) %>%
-  step_normalize(all_predictors()) %>%
-  step_pca(all_predictors(), threshold = 0.75)
+#### NMDS Samples ####
+library(vegan)
 
-var_exp <- prep(pca_results) %>%
-  variance_explained(4)
+nmds <- metaMDS(select(prepared_dataset, -final_disease_state) %>%
+          column_to_rownames('genotype'))
 
-prep(pca_results) %>%
-  tidy(4) %>%
-  filter(component %in% str_c('PC', 1:6)) %>%
-  group_by(component) %>%
-  top_n(20, abs(value)) %>%
-  ungroup() %>%
-  mutate(terms = reorder_within(terms, abs(value), component)) %>%
-  ggplot(aes(abs(value), terms, fill = value > 0)) +
-  geom_col() +
-  facet_wrap(~component, scales = "free_y") +
-  scale_y_reordered() +
-  labs(x = "Absolute value of contribution",
-       y = NULL, fill = "Positive?")
+plot(nmds)
+
+corr_asvs <- envfit(nmds, select(prepared_dataset, -final_disease_state) %>%
+         column_to_rownames('genotype'))
 
 
-significant_associations <- prep(pca_results) %>%
-  pca_correlations_original_var() %>%
-  group_by(component) %>%
-  mutate(fdr = p.adjust(p, method = 'fdr')) %>%
-  ungroup %>%
-  filter(fdr < 0.05) %>%
-  filter(component %in% str_c('PC', 1:2)) %>%
-  pull(terms)
-
-variable_loadings <- prep(pca_results) %>%
-  tidy(4, matrix = "rotation") %>%
-  filter(component %in% str_c('PC', 1:2)) %>%
-  pivot_wider(names_from = "component", 
-              values_from = "value") %>%
-  filter(terms %in% significant_associations) %>%
-  filter(str_detect(terms, 'asv')) %>%
-  # select(terms) %>%
-  mutate(time = str_extract(terms, '^T[037]'),
-         asv_id = str_extract(terms, 'ASV_[0-9]+')) %>%
-  select(-terms) %>%
+asv_nmds <- scores(nmds, 'species') %>%
+  as_tibble(rownames = 'asv_id') %>%
+  filter(asv_id %in% names(corr_asvs$vectors$pvals[corr_asvs$vectors$pvals < 0.05])) %>%
+  mutate(time = str_extract(asv_id, '^T[037]'),
+         asv_id = str_extract(asv_id, 'ASV_[0-9]+')) %>%
   left_join(normalized_asv_counts %>%
               select(Order:Genus, asv_id) %>%
               distinct,
-            by = 'asv_id') 
+            by = 'asv_id') %>%
+  mutate(asv_id = str_c(time, asv_id, sep = ': '))
 
-prep(pca_results) %>%
-  juice() %>%
-  ggplot(aes(x = PC1, y = PC2)) +
+scores(nmds, 'sites') %>%
+  as_tibble(rownames = 'genotype') %>%
+  left_join(select(samples_through_time, genotype, final_disease_state),
+            by = 'genotype') %>%
+  ggplot(aes(x = NMDS1, y = NMDS2)) +
   
-  geom_segment(data = variable_loadings %>% mutate(across(starts_with('PC'), ~. * 50)),
-               aes(linetype = time, colour = Family),
-               xend = 0, yend = 0, 
-               arrow = arrow(angle = 20, ends = "first", 
-                             type = "closed", 
-                             length = grid::unit(8, "pt"))) +
-  geom_text(data = variable_loadings %>% mutate(across(starts_with('PC'), ~. * 50)),
-                  aes(colour = Family, label = asv_id), show.legend = FALSE) +
+  geom_point(data = asv_nmds,
+             aes(colour = Family), size = 1) +
+  geom_text(data = asv_nmds,
+            aes(label = asv_id, colour = Family), 
+            check_overlap = TRUE, hjust = "inward", 
+            size = 3) +
   
-  geom_point(aes(shape = final_disease_state), alpha = 0.7, size = 2) +
+  geom_point(aes(shape = final_disease_state), alpha = 1, size = 2) +
   geom_text(aes(label = genotype), check_overlap = TRUE, hjust = "inward") +
-  labs(color = NULL,
-       x = str_c('PC1 (', scales::percent(var_exp[1]), ')'),
-       y = str_c('PC2 (', scales::percent(var_exp[2]), ')'))
-
+  guides(colour = guide_legend(override.aes = list(size = 2))) +
+  labs(color = 'ASV Family',
+       x = str_c('NMDS1'),
+       y = str_c('NMDS2'),
+       shape = 'Disease') +
+  theme_classic() +
+  theme(axis.text = element_text(colour = 'black'),
+        axis.title = element_text(colour = 'black', size = 14),
+        panel.background = element_rect(colour = 'black'))
+ggsave('../Figures/nmds_diseaseExposed_genotype_asv.png', height = 10, width = 10)
 
 #### Train/Test Split ####
 # not ideal because of sample size...but what can you do
@@ -182,8 +163,6 @@ coral_test <- testing(coral_split)
 count(coral_train, final_disease_state)
 count(coral_test, final_disease_state)
 
-#### Random Forest of all data points ####
-
 #### Random Forest Preprocessing ####
 preprocess_recipie <- recipe(final_disease_state ~ ., data = coral_train) %>%
   update_role(genotype, new_role = "ID") %>%
@@ -191,18 +170,11 @@ preprocess_recipie <- recipe(final_disease_state ~ ., data = coral_train) %>%
   step_nzv(all_predictors()) %>%
   step_log(all_predictors(), base = 2, offset = 0.5) %>%
   step_normalize(all_predictors()) %>%
-  
-  # step_pca(all_predictors(), threshold = 0.75) %>%
   identity()
 
-prep(preprocess_recipie) %>%
-  juice()
-
-
 #### Build Random Forest Model ####
-#not enough samples for any real parameter tuning 
 forest_model <- rand_forest() %>%
-  set_engine('ranger', importance = "impurity") %>%
+  set_engine('ranger', importance = 'permutation') %>%
   set_mode('classification') %>%
   set_args(mtry = tune(),
            trees = tune(),
@@ -214,27 +186,48 @@ disease_wflow <- workflow() %>%
 
 #### Train Hyperparameters ####
 coral_folds <- vfold_cv(coral_train, 
-                        v = 3, repeats = 5, 
+                        v = 3, repeats = 10, 
                         strata = final_disease_state)
 
-tuning_params <- parameters(list(mtry = mtry(range = c(50, 500)), 
-                                 trees = trees(range = c(1e3, 1e4)),
+tuning_params <- parameters(list(mtry = mtry(range = c(10, 250)), 
+                                 trees = trees(range = c(1e2, 1e4)),
                                  min_n = min_n(range = c(1, 4))))
 
 
-tune_aov <- tune_race_anova(disease_wflow,
-                            resamples = coral_folds,
-                            param_info = tuning_params,
-                            metrics = metric_set(accuracy, roc_auc),
-                            grid = 20,
-                            control = control_race(verbose = TRUE,
-                                                   verbose_elim = TRUE))
-autoplot(tune_aov)
+# tune_aov <- tune_race_anova(disease_wflow,
+#                             resamples = coral_folds,
+#                             param_info = tuning_params,
+#                             metrics = metric_set(accuracy),
+#                             grid = 20,
+#                             control = control_race(verbose = TRUE,
+#                                                    verbose_elim = TRUE,
+#                                                    alpha = 0.1,
+#                                                    burn_in = 5))
+# autoplot(tune_aov)
 
+initial_grid_tune <- tune_grid(disease_wflow,
+                               resamples = coral_folds,
+                               param_info = tuning_params,
+                               metrics = metric_set(accuracy),
+                               grid = 10,
+                               control = control_grid(verbose = TRUE))
+
+bayes_tune <- tune_bayes(disease_wflow,
+                         resamples = coral_folds,
+                         param_info = tuning_params,
+                         metrics = metric_set(accuracy),
+                         iter = 100,
+                         initial = initial_grid_tune,
+                         objective = exp_improve(),
+                         control = control_bayes(verbose = TRUE,
+                                                 verbose_iter = TRUE,
+                                                 no_improve = 10,
+                                                 uncertain = 5))
+autoplot(bayes_tune)
 
 #### Finalize Hyper Parameters ####
 final_rf <- finalize_workflow(disease_wflow,
-  select_best(tune_aov, "accuracy"))
+  select_best(bayes_tune, "accuracy"))
 
 #### Assess Model Accuracy on Test Dataset ####
 disease_fit <- final_rf %>%
@@ -246,28 +239,84 @@ disease_fit %>%
 disease_fit %>%
   collect_predictions()
 
-#### Get Variable Importance ####
-final_rf %>%
-  fit(data = prepared_dataset) %>%
-  extract_fit_parsnip() %>%
-  vip(geom = "point",
-      num_features = 10)
+#### Fit Final Model for Variable Importance ####
+fit_rf <- final_rf %>%
+  fit(data = prepared_dataset)
 
-#### Plot Counts of top ASVs ####
-final_rf %>%
-  fit(data = prepared_dataset) %>%
+#### Get Variable Importance ####
+fit_rf %>%
   extract_fit_parsnip() %>% 
   vi() %>%
-  arrange(-Importance) %>%
-  slice(1:10) %>%
-  mutate(asv_id = str_extract(Variable, 'ASV_[0-9]+')) %>%
+  mutate(asv_id = str_extract(Variable, 'ASV_[0-9]+'),
+         importance_time = str_extract(Variable, '^T[037]')) %>%
+  group_by(importance_time) %>%
+  slice_max(n = 12, order_by = Importance) %>%
+  ungroup %>%
+  filter(Importance > 0) %>%
+  mutate(Variable = reorder_within(Variable, Importance, 
+                                   within = importance_time)) %>%
+  ggplot(aes(x = Importance, y = Variable)) +
+  geom_segment(xend = 0, aes(yend = Variable)) +
+  geom_point(size = 2) +
+  scale_y_reordered() +
+  facet_wrap(~importance_time, scales = 'free_y',
+             ncol = 1) +
+  theme_classic() +
+  labs(y = NULL) +
+  theme(panel.background = element_rect(colour = 'black'),
+        strip.background = element_blank())
+ggsave('../Figures/random_forest_asvImportance.png', height = 15, width = 7)
+
+#### SHAP Plots ####
+library(DALEXtra)
+#https://ema.drwhy.ai/shapley.html#SHAPRcode
+# https://www.tmwr.org/explain.html#back-to-beans
+# https://christophm.github.io/interpretable-ml-book/shap.html
+accuracy_loss <- function(observed, predicted){
+  mean(observed == predicted)
+}
+
+tmp <- explain_tidymodels(fit_rf, 
+                   data = select(prepared_dataset, -final_disease_state), 
+                   y = prepared_dataset$final_disease_state == 'D',
+                   verbose = TRUE)
+
+loss_default(tmp$model_info$type)
+
+blat <- predict_parts(explainer = tmp, 
+              new_observation = prepared_dataset, 
+              type = "shap",
+              B = 25)
+
+tst <- model_parts(tmp)
+plot(tst, max_vars = 10)
+tst$variable[1]
+
+#### Plot Counts of top ASVs ####
+fit_rf %>%
+  extract_fit_parsnip() %>% 
+  vi() %>%
+  mutate(asv_id = str_extract(Variable, 'ASV_[0-9]+'),
+         importance_time = str_extract(Variable, '^T[037]')) %>%
+  slice_max(n = 12, order_by = Importance) %>%
+  ungroup %>%
+  filter(Importance > 0) %>%
+  ungroup %>%
   filter(!is.na(asv_id)) %>%
   left_join(normalized_asv_counts,
-            by = 'asv_id') %>%
-  mutate(asv_id = str_c(Family, Genus, asv_id, sep = ': ')) %>%
+            by = 'asv_id', relationship = 'many-to-many') %>%
+  mutate(asv_id = str_c(Family, Genus, asv_id, sep = ': '),
+         asv_id = fct_reorder(asv_id, -Importance)) %>%
   
   ggplot(aes(x = time, y = log2_cpm, colour = exposure,
              shape = susceptability)) +
   stat_summary(fun.data = mean_se, position = position_dodge(0.5)) +
-  facet_wrap(~asv_id)
- 
+  facet_wrap( ~ asv_id) +
+  labs(y = 'log2(cpm)',
+       x = NULL,
+       colour = 'Exposure',
+       shape = 'Susceptability') +
+  theme_classic() +
+  theme(panel.background = element_rect(colour = 'black'),
+        strip.background = element_blank())
+ggsave('../Figures/rf_important_asv_time.png', height = 15, width = 15)
