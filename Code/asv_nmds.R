778d5d9e783b78e183aef1ccbfc99acfce1b2323
emilytrytten <trytten.e@northeastern.edu>
2023-07-06 16:14:15 -0400

    updated nmds

--- Code/asv_nmds.R
+++ Code/asv_nmds.R
@@ -1,4 +1,20 @@
 
+tgfff <- bacterial_signature_asv %>% 
+  group_by(asv_id) %>% 
+  reframe(bacstrat = paste(list(signatures))) %>%
+  mutate(bacstrat = ifelse(bacstrat == 
+                             'c("late_pathogen", "late_opportunist")', "late_opportunist&pathogen", 
+                           bacstrat)) %>%
+  mutate(bacstrat = ifelse(bacstrat == 
+                             'c("early_opportunist", "continuous_opportunist", "late_opportunist")', 
+                           "continuous_opportunist", bacstrat)) %>%
+  mutate(bacstrat = ifelse(bacstrat == 
+                             'c("early_pathogen", "late_opportunist")', 
+                           "early_path_late_opp", bacstrat)) %>%
+  mutate(bacstrat = ifelse(bacstrat == 
+                             'c("early_pathogen", "early_opportunist", "continuous_opportunist", "late_opportunist")', 
+                           "early_path_cont_opp", bacstrat))
+
 
 testnmds <- full_data %>% 
   mutate(category = paste(time, exposure, susceptability, sep = "_"), .after = asv_id) %>%
@@ -7,6 +23,9 @@ testnmds <- full_data %>%
   summarize(ave_cpm = mean(log2_cpm)) %>%
   mutate(ave_cpm = str_trim(as.numeric(ave_cpm))) %>%
   pivot_wider(names_from = category, values_from = ave_cpm) %>%
+  left_join(tgfff, by = join_by("asv_id")) %>%
+  filter(!is.na(bacstrat)) %>%
+  select(-bacstrat) %>%
   as.matrix()
 
 mat_nmds <- as.matrix(testnmds[, -1])
@@ -21,40 +40,37 @@ colnames(test_num_mat) <- colnames(mat_nmds)
 dist.f <- vegdist(test_num_mat, method = "bray")
 
 test_nmds <- metaMDS(dist.f, distance = 'bray', k = 2, trymax = 100, autotransform = FALSE, verbose = TRUE)
-sppscores(test_nmds) <- test5
+sppscores(test_nmds) <- test_num_mat
 
-tgfff <- bacterial_signature_asv %>% 
-  group_by(asv_id) %>% 
-  reframe(bacstrat = paste(list(signatures))) %>%
-  mutate(bacstrat = ifelse(bacstrat == 
-        'c("late_opportunist", "late_pathogen")', "late_opportunist&pathogen", 
-        bacstrat)) %>%
-  mutate(bacstrat = ifelse(bacstrat == 
-        'c("early_opportunist", "continuous_opportunist", "late_opportunist")', 
-        "continuous_opportunist", bacstrat))
- 
+
+
 #make plot
-  scores(test_nmds)$sites %>%
+scores(test_nmds)$sites %>%
   as_tibble(rownames = 'asv_names') %>%
   #left_join(metadata, by = 'asv_names') %>%
   left_join(tgfff, by = c('asv_names' = 'asv_id')) %>%
-  mutate(bacstrat = ifelse(is.na(bacstrat), "disease_unrelated", bacstrat)) %>%
+  filter(!is.na(bacstrat)) %>%
+  #mutate(bacstrat = ifelse(is.na(bacstrat), "disease_unrelated", bacstrat)) %>%
   mutate(shape_determination = ifelse(str_detect(bacstrat, "disease"), 
-        "disease_unrelated", ifelse(str_detect(bacstrat, "pathogen"), "pathogen", 
+        "disease_unrelated", ifelse(str_detect(bacstrat, "path"), "pathogen", 
         "opportunist"))) %>%
   ggplot(aes(x = NMDS1, y = NMDS2)) +
   geom_point(data = as_tibble(scores(test_nmds)$species, rownames = aggregation_level) %>% mutate(timepoint = str_extract_numbers(none)),
-             size = 1, shape = 1) +
-  geom_point(aes(alpha = shape_determination, col = bacstrat, shape = shape_determination)) +
-  scale_alpha_manual(values = c(0.3, 1,1)) +
+             size = 1, shape = 1, color = "gray90") +
+  geom_text(data = as_tibble(scores(test_nmds)$species, rownames = aggregation_level) %>% mutate(timepoint = str_extract_numbers(none)),
+              aes(label = none), size = 3) +
+  geom_point(aes(col = bacstrat, shape = shape_determination), size = 3) + #alpha = shape_determination
+  #scale_alpha_manual(values = c(0.3, 1,1)) +
   scale_color_manual(values = c(
-    "early_pathogen" = "pink2",
-    "late_pathogen" = "red",
-    "early_opportunist" = "lightskyblue1",
-    "late_opportunist" = "mediumblue",
+    "early_pathogen" = "#FF7575",
+    "late_pathogen" = "#CF0C0C",
+    "early_opportunist" = "lightskyblue",
+    "late_opportunist" = "deepskyblue3",
     "late_opportunist&pathogen" = "purple",
-    "continuous_opportunist" = "deepskyblue",
-    "disease_unrelated" = "gray"
+    "continuous_opportunist" = "royalblue4",
+    "early_path_cont_opp" = "hotpink3",
+    "early_path_late_opp" = "orange"
+    #"disease_unrelated" = "gray"
   )) +
   theme_bw()
 
